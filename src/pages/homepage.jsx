ed8e569cffff98d941a6bcaa701321f8b93ea9e8
Narrative-footy <narrativefooty@gmail.com>
2026-04-29 18:23:09 -0400

    scoopers

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,7 +1,9 @@
 import axios  from "axios";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
+    const navigate = useNavigate()
     const [score, setScore] = useState(0);
     const [totalScore, setTotalScore] = useState(0);
     const [team, setTeam] = useState([]);
@@ -15,9 +17,9 @@ export default function Homepage() {
             <h1>Prem Predictor</h1>
             <h2> Select a type of bot</h2>
             <div>
-                <button onClick={redirectTeam("view_information")}> View Information </button>
-                <button onClick={redirectTeam("statistician")}> Statistician </button>
-                <button onClick={redirectTeam("defensive_minded")}> Defensive Minded</button>
+                <button onClick={() => redirectTeam("view_information")}> View Information </button>
+                <button onClick={() => redirectTeam("statistician")}> Statistician </button>
+                <button onClick={() => redirectTeam("defensive_minded")}> Defensive Minded</button>
             </div>
         </div>
         </>
