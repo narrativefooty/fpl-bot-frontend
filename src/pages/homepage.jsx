afe30b601c9b18bdafbcb39e3efbfd0285cbf33a
Narrative-footy <narrativefooty@gmail.com>
2026-04-29 18:01:53 -0400

    mitter

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,25 +6,12 @@ export default function Homepage() {
     const [totalScore, setTotalScore] = useState(0);
     const [team, setTeam] = useState([]);
 
-    useEffect(() => {
-        axios.get("fpl-bot-backend.vercel.app/api/home").then((teams)=>{
-            setTeam(teams.data.team)
-            setScore(teams.data.week)
-            setTotalScore(teams.data.total)
-        })
-        .catch(err=>console.log(err))
-    }, [])
 
     return(
         <>
         <div>
             <h1>Homepage</h1>
-            {team.map((player, index) => (
-                <div key={index}>
-                    <p>{player.name}</p>
-                    <p>{player.score}</p>
-                </div>
-            ))}
+
         </div>
         </>
     )
