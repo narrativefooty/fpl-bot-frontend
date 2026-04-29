abfde9df4bf264ee0f6b1a2e4baf4359a7c5a8d1
Narrative-footy <narrativefooty@gmail.com>
2026-04-29 18:17:00 -0400

    sparks

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,12 +6,17 @@ export default function Homepage() {
     const [totalScore, setTotalScore] = useState(0);
     const [team, setTeam] = useState([]);
 
-
+    
     return(
         <>
         <div>
-            <h1>Homepage</h1>
-
+            <h1>Prem Predictor</h1>
+            <h2> Select a type of bot</h2>
+            <div>
+                <button> View Information </button>
+                <button> Statistician </button>
+                <button> Defensive Minded</button>
+            </div>
         </div>
         </>
     )
