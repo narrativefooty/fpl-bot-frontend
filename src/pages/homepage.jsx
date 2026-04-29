831afa261bb86f6f828b3367294dff4eda3c9646
Narrative-footy <narrativefooty@gmail.com>
2026-04-29 18:20:11 -0400

    defensive

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,16 +6,18 @@ export default function Homepage() {
     const [totalScore, setTotalScore] = useState(0);
     const [team, setTeam] = useState([]);
 
-    
+    function redirectTeam(type) {
+        navigate(`/predictor/${type}`);
+    }
     return(
         <>
         <div>
             <h1>Prem Predictor</h1>
             <h2> Select a type of bot</h2>
             <div>
-                <button> View Information </button>
-                <button> Statistician </button>
-                <button> Defensive Minded</button>
+                <button onClick={redirectTeam("view_information")}> View Information </button>
+                <button onClick={redirectTeam("statistician")}> Statistician </button>
+                <button onClick={redirectTeam("defensive_minded")}> Defensive Minded</button>
             </div>
         </div>
         </>
