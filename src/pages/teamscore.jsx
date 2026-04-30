4e4be8cf214dac746078bdba0df30ef4faa2c9df
Narrative-footy <narrativefooty@gmail.com>
2026-04-30 14:06:34 -0400

    squabble

---
+++ src/pages/teamscore.jsx
@@ -0,0 +1,36 @@
+import axios  from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate,useParams } from "react-router-dom";
+
+export default function Teamscore() {
+    const {type} = useParams();
+    const navigate = useNavigate()
+    const [score, setScore] = useState(0);
+    const [totalScore, setTotalScore] = useState(0);
+    const [starters, setStarters] = useState([]);
+    const [benched, setBenched] = useState([]);
+
+    useEffect(() => {
+    axios.get(`https://predictor-backend-nine.vercel.app/api/scores/${type}`, {headers: { Authorization:`Bearer ${localStorage.getItem("token")}`}})
+    .then(res => {
+        setStarters(res.data.starters);
+        setBenched(res.data.submitted);
+        setLocked(res.data.locked);
+        setTotalScore(res.data.total);
+        setScore(res.data.score)
+    })
+    .catch(err => console.log("API Error: ",err));
+    }, [type]);
+    
+    return(
+        <>
+        <div>
+            <h1>Prem Predictor</h1>
+            <h2> { type } </h2>
+            <div>
+                
+            </div>
+        </div>
+        </>
+    )
+}
