665a508ad2025a90e33dd77f2e853e79783a61b6
Narrative-footy <narrativefooty@gmail.com>
2026-05-05 14:48:38 -0400

    proj

--- src/pages/teamscore.jsx
+++ src/pages/teamscore.jsx
@@ -37,6 +37,8 @@ export default function Teamscore() {
             <h2> { type } </h2>
             {Array.isArray(starters) && starters.length > 0 ? (
             <div id = "gk">
+                <h1> Total Score : {totalScore}</h1>
+                <h1> Score : {totalScore}</h1>
                 <div className="player-box">
                     <div>
                     <h1>{starters.find(p=>p.position == 1 && p.bench_order == 0)?.name}</h1>
